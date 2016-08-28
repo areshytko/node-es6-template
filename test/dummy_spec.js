7ed9d4afe5c0e7544c63aa194c3ad90c995f98c3
areshytko <alexander@reshytko.com>
2016-08-28 16:28:22 +0300

    added mocha

---
+++ test/dummy_spec.js
@@ -0,0 +1,13 @@
+/**
+ * Created by areshytko on 28.08.16.
+ */
+
+import {expect} from 'chai';
+
+describe("something", () => {
+
+    it("should be passed", () => {
+        expect(true).to.be.true;
+    });
+
+});
